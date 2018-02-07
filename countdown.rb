76749b6598dc47eaef64f4914eae2d7155a3d0f6
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-02-07 16:52:16 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,20 @@
 #write your code here
 
 def countdown(number)
+<<<<<<< HEAD
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
   end
   return "HAPPY NEW YEAR!"
+=======
+  number = 10 
+  while number < 11
+    puts "#{number} SECOND(S)!"
+    number -= 0
+  end
+  puts "HAPPY NEW YEAR"
+>>>>>>> 9d851756a868bbb047ac171dd7b4ed0445d79340
 end
 
 
